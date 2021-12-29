3516e8ff3d271b8b8b9e8e84a2279f37effc3b06
jkkCoder <jayeshk118@gmail.com>
2021-12-29 16:08:31 +0530

    commented production code in app.js and added alternate code to fix an error

--- app.js
+++ app.js
@@ -21,10 +21,16 @@ app.use(require("./routes/post"))
 app.use(require("./routes/user"))
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static("client/build "))
-    const path = require("path")
+    // app.use(express.static("client/build "))
+    // const path = require("path")
+    // app.get("*",(req,res)=>{
+    //     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    // })
+
+    const root = require("path").join(__dirname,"client","build")
+    app.use(express.static(root))
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+        res.sendFile("index.html",{root})
     })
 }
 
